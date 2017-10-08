eb86545c223cded91526d020f324ab9c09e82a36
demoemailid1101 <demo.emailid1101@gmail.com>
2017-10-08 14:14:10 +0530

    -fix usefactory error in app.module. -complete profile edit module.

--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 import { ThemeModule } from '../../@theme/theme.module';
 import { ProfileRoutingModule, routedComponents } from './profile-routing.module';
 import { ProfileService } from './profile.service';
-//import { NgUploaderModule } from 'ngx-uploader';
+import { NgUploaderModule } from 'ngx-uploader';
 // import { UserService } from './user.service';
 
 @NgModule({
   imports: [
     ThemeModule,
     ProfileRoutingModule,
- //   NgUploaderModule,
+    NgUploaderModule,
   ],
   declarations: [
     ...routedComponents,
